ea393016cea5fc1ba6b267c1e0f50707bdf29173
Anish Srivastava <anish_srivastava@epam.com>
2024-08-19 16:32:19 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,3 @@
+const element = React.createElement("h1",{id:"heading"},"first word from React seprate file");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(element);
